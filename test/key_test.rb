82cdbdb3ba10a64ec9f3e119b1004580f01e7fc2
Ryan Laleh <laleh21@yahoo.com>
2020-06-05 18:29:54 -0600

    Add test to assign keys. Expecting hash

--- test/key_test.rb
+++ test/key_test.rb
@@ -10,8 +10,15 @@ class KeyTest < Minitest::Test
 
   def test_it_can_generate_5_random_numbers
     @key = Key.new
-    # @key.expects(:random_digit_generator).returns("10247")
+    @key.expects(:random_digit_generator).returns("10247")
     assert_equal "10247", @key.random_digit_generator
   end
 
+  def test_it_can_assign_keys
+    @key = Key.new
+    @key.expects(:random_digit_generator).returns("10247")
+
+    expected = ({A: 10, B: 02, C: 24, D: 47})
+    assert_equal expected, @key.assign_keys
+  end
 end
