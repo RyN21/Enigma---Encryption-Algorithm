9c41532f6df838985e233bdae0aa7857623f0531
Ryan Laleh <laleh21@yahoo.com>
2020-06-05 19:36:04 -0600

    Add test to pair 5 digits

--- lib/key.rb
+++ lib/key.rb
@@ -1,15 +1,30 @@
 class Key
-  attr_reader :assigned
+  attr_reader :keys
+
   def initialize
-    @assigned = Hash.new(0)
+    @keys = {A: 0, B: 0, C: 0, D: 0}
+    @random_5digits = []
   end
-  def random_digit_generator
-    5.times.map do
-      rand(5)
+
+  def random_5digits
+    @random_5digits = 5.times.map do
+      rand(9)
     end
   end
 
-  def assign_keys
-    # require "pry"; binding.pry
-  end
+
+  # def assign_keys(random_5digits)
+  #   keys.each do |key, value|
+  #     until dsfasd
+  #     end
+  #   end
+  # end
+
+  # def assign_keys
+  #   require "pry"; binding.pry
+  #   keys[:A] = assign_keys[0,1]
+  #   keys[:A] = assign_keys[0,1]
+  #   keys[:A] = assign_keys[0,1]
+  #   keys[:A] = assign_keys[0,1]
+  # end
 end
--- test/key_test.rb
+++ test/key_test.rb
@@ -6,19 +6,25 @@ class KeyTest < Minitest::Test
     @key = Key.new
 
     assert_instance_of Key, @key
-    assert_equal ({}), @key.assigned
+    assert_equal ({A: 0, B: 0, C: 0, D: 0}), @key.keys
   end
 
   def test_it_can_generate_5_random_numbers
     @key = Key.new
-    @key.expects(:random_digit_generator).returns([1, 0, 2, 4 ,7])
-    assert_equal [1, 0, 2, 4, 7], @key.random_digit_generator
+    @key.expects(:random_5digits).returns([1, 0, 2, 4 ,7])
+    assert_equal [1, 0, 2, 4, 7], @key.random_5digits
+  end
+
+  def test_it_can_pair_5_digits
+    @key = Key.new
+    # @key.expects(:random_5digits).returns([1, 0, 2, 4 ,7])
+    assert_equal [[1,0] [0,2] [2,4] [4,7]], @key.pair_digits
   end
 
   def test_it_can_assign_keys
     skip
     @key = Key.new
-    @key.expects(:random_digit_generator).returns([1, 0, 2, 4 ,7])
+    # @key.expects(:random_5digits).returns([1, 0, 2, 4 ,7])
 
     expected = ({A: 10, B: 02, C: 24, D: 47})
     assert_equal expected, @key.assign_keys
