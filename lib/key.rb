2168d3f7848de61c67e6b807fb65ffa4c06cfd35
Ryan Laleh <laleh21@yahoo.com>
2020-06-05 18:33:56 -0600

    Add assigned as an attribute that returns an empty hash to start

--- lib/key.rb
+++ lib/key.rb
@@ -1,8 +1,12 @@
 class Key
-
+  
   def random_digit_generator
     5.times.map do
       rand(5)
     end
+  end
+
+  def assign_keys
+    require "pry"; binding.pry
   end
 end
--- test/key_test.rb
+++ test/key_test.rb
@@ -6,17 +6,18 @@ class KeyTest < Minitest::Test
     @key = Key.new
 
     assert_instance_of Key, @key
+    assert_instance_of ({}), @key.assigned
   end
 
   def test_it_can_generate_5_random_numbers
     @key = Key.new
-    @key.expects(:random_digit_generator).returns("10247")
+    @key.expects(:random_digit_generator).returns([1, 0, 2, 4 ,7])
     assert_equal "10247", @key.random_digit_generator
   end
 
   def test_it_can_assign_keys
     @key = Key.new
-    @key.expects(:random_digit_generator).returns("10247")
+    @key.expects(:random_digit_generator).returns([1, 0, 2, 4 ,7])
 
     expected = ({A: 10, B: 02, C: 24, D: 47})
     assert_equal expected, @key.assign_keys
