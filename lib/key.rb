a5ffa27aed362277e790281761e7264a09924670
Ryan Laleh <laleh21@yahoo.com>
2020-06-05 20:43:47 -0600

    Add assign_keys method. Finish key class

--- lib/key.rb
+++ lib/key.rb
@@ -1,9 +1,9 @@
 class Key
-  attr_reader :keys, :digits
+  attr_reader :keys, :split_digits
 
   def initialize
     @keys = {A: 0, B: 0, C: 0, D: 0}
-    @digits = []
+    @split_digits = []
   end
 
   def random_5digits
@@ -12,17 +12,16 @@ class Key
     end
   end
 
-  def pair_digits(digit)
+  def pair_digits(digits)
     4.times do
-      @digits << digit[0..1].join
-      digit.shift
+      @split_digits << digits[0..1].join
+      digits.shift
     end
   end
 
-  # def assign_keys(digits)
-  #   keys.each do |key|
-  #     key = digits[0..1].join
-  #     digits.shift
-  #   end
-  # end
+  def assign_keys
+    @keys.transform_values do |key|
+      key = split_digits.shift
+    end
+  end
 end
--- test/key_test.rb
+++ test/key_test.rb
@@ -17,18 +17,18 @@ class KeyTest < Minitest::Test
 
   def test_it_can_pair_digits
     @key = Key.new
-    # @key.expects(:random_5digits).returns(['1', '0', '2', '4', '7'])
+    @key.expects(:random_5digits).returns(['1', '0', '2', '4', '7'])
     @key.pair_digits(@key.random_5digits)
 
-    assert_equal ['10', '02', '24', '47'], @key.digits
+    assert_equal ['10', '02', '24', '47'], @key.split_digits
   end
 
   def test_it_can_assign_keys
-    skip
     @key = Key.new
     @key.expects(:random_5digits).returns([1, 0, 2, 4 ,7])
+    @key.pair_digits(@key.random_5digits)
 
-    expected = ({A: 10, B: 02, C: 24, D: 47})
-    assert_equal expected, @key.assign_keys(@key.digits)
+    expected = ({A: '10', B: '02', C: '24', D: '47'})
+    assert_equal expected, @key.assign_keys
   end
 end
