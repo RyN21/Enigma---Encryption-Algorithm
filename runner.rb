fedb0646d17fcc7c6e46d929132dbdc28c7a5b5b
Ryan Laleh <laleh21@yahoo.com>
2020-06-08 10:51:51 -0600

    Cleaned up files

--- runner.rb
+++
@@ -1,8 +0,0 @@
-@enigma = Enigma.new
-@key = Key.new
-@offset = Offset.new
-@shift = Shift.new(@key, @offset)
-@key.expects(:random_5digits).returns(['1', '0', '2', '4', '7'])
-@key.assign_keys
-@offset.assign_offsets
-@shift.make_shift_keys
--- test/encrypt_test.rb
+++
@@ -1,6 +0,0 @@
-require './test/test_helper'
-require './lib/enigma'
-
-class EncryptTest < Minitest::Test
-  
-end
