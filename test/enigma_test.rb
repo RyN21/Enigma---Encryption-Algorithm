58fbd6ae2f751b102b6d83554808099319307d55
Ryan Laleh <laleh21@yahoo.com>
2020-06-08 06:55:11 -0600

    Start building tests for enigma class. Does it exist and has attributes

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -7,41 +7,41 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, @enigma
   end
 
-  def test_it_can_encyrpt
-    skip
-    expected = ({
-      encryption: "keder ohulw",
-      key: "02715",
-      date: "040895"
-    })
-
-    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
-  end
-
-  def test_it_can_decrypt
-    skip
-    expected = {
-      decryption: "hello world",
-      key: "02715",
-      date: "040895"
-    }
-
-    assert_equal expected, @enigma.decrypt("keder ohulw", "02715", "040895")
-  end
-
-  def test_it_can_encyrpt_with_todays_date
-    skip
-    @encrypted = @enigma.encrypt("hello world", "02715")
-    assert_equal expected, @enigma.encrypt("hello world", "02715")
-  end
-
-  def test_it_can_decyrpt_with_todays_date
-    skip
-    assert_equal expected, @enigma.decrypt(encrypted[:encryption], "02715")
-  end
-
-  def test_it_can_encrypt_with_random_key_and_todays_date
-    skip
-    assert_equal expected, @enigma.encrypt("hello world")
-  end
+  # def test_it_can_encyrpt
+  #   skip
+  #   expected = ({
+  #     encryption: "keder ohulw",
+  #     key: "02715",
+  #     date: "040895"
+  #   })
+  #
+  #   assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
+  # end
+  #
+  # def test_it_can_decrypt
+  #   skip
+  #   expected = {
+  #     decryption: "hello world",
+  #     key: "02715",
+  #     date: "040895"
+  #   }
+  #
+  #   assert_equal expected, @enigma.decrypt("keder ohulw", "02715", "040895")
+  # end
+  #
+  # def test_it_can_encyrpt_with_todays_date
+  #   skip
+  #   @encrypted = @enigma.encrypt("hello world", "02715")
+  #   assert_equal expected, @enigma.encrypt("hello world", "02715")
+  # end
+  #
+  # def test_it_can_decyrpt_with_todays_date
+  #   skip
+  #   assert_equal expected, @enigma.decrypt(encrypted[:encryption], "02715")
+  # end
+  #
+  # def test_it_can_encrypt_with_random_key_and_todays_date
+  #   skip
+  #   assert_equal expected, @enigma.encrypt("hello world")
+  # end
 end
